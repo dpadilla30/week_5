566687777fdf9b349e55015081c109dfe4aec7ac
Padilla <dpadilla30@cps.k12.il.us>
2024-09-26 08:32:32 -0500

    first commit

---
+++ sets_tuples_lists.py/sets_tuples_lists.py
@@ -0,0 +1,25 @@
+#collection = single "variable" used to store multiple values
+#list = [] ordered and changeable. duplicates OK
+# set =() unordered and imutable, but add/remove OK. NO dupelicates
+#tuple=() ordered and unchangeable. Duplicates OK. FASTER
+
+fruits=["apple","orange","banana","coconut","kiwi","strawberry"]
+#print(dir(fruits))# tells us all the methods that come with it
+#print(help(fruits))#
+#print(len(fruits))# tells us the length
+#print("pineapple" in fruits) #tells us if pineapple is in fruits
+
+fruits[0]="pineapple" #Reasign values
+
+#print(fruits[0])
+#print(fruits[4])
+
+#fruits[0]="pineapple"
+#fruits.append("pineapple")
+#fruits.remove("apple")
+#fruits.insert(0,"pineapple")
+#fruits.sort()
+#fruits.reverse()
+#fruits.clear()
+
+#for x in fruits: print(x)
