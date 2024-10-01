ccee60bbd7b69ad784f8d356c4d13131b12e7bcf
Padilla <dpadilla30@cps.k12.il.us>
2024-10-01 09:01:48 -0500

    first commit

--- sets_tuples_lists.py/sets_tuples_lists.py
+++ sets_tuples_lists.py/sets_tuples_lists.py
@@ -1,9 +1,9 @@
 #collection = single "variable" used to store multiple values
 #list = [] ordered and changeable. duplicates OK
-# set =() unordered and imutable, but add/remove OK. NO dupelicates
+# set ={} unordered and imutable, but add/remove OK. NO dupelicates
 #tuple=() ordered and unchangeable. Duplicates OK. FASTER
-
-fruits=["apple","orange","banana","coconut","kiwi","strawberry"]
+#lists
+#fruits=["apple","orange","banana","coconut"]
 #print(dir(fruits))# tells us all the methods that come with it
 #print(help(fruits))#
 #print(len(fruits))# tells us the length
@@ -62,27 +62,100 @@ fruits=["apple","orange","banana","coconut","kiwi","strawberry"]
     #challenge
     #create a list of friends
     #make sure the initial list is none
-friends=[]
+#friends=[]
     #add a new friend to the lsit, add at least 5 friends
-friends.append("Abel")
-friends.append("Lorenzo")
-friends.append("Angel")
-friends.append("Alex")
-friends.append("Oscar")
+#friends.append("Abel")
+#friends.append("Lorenzo")
+#friends.append("Angel")
+#friends.append("Alex")
+#friends.append("Oscar")
     #remove a friend
-friends.remove("Abel")
+#friends.remove("Abel")
     #insert a friend at a specifi index maybe 2
-friends[2]="Abel"
+#friends[2]="Abel"
     #print the list of friends
     
     
         #loop through the lsit and print the friends name
-for friend in friends:
-            friendMore= input("add a new friend please:")
-            friends.append(friendMore)
-            print(friends)
+#for friend in friends:
+            #friendMore= input("add a new friend please:")
+            #friends.append(friendMore)
+            #print(friends)
          #see if a particular friend is in the list(boolean value)
-            print("Abel"in friends)
+            #print("Abel"in friends)
             #if the lsit is grater than 10 break the loop
-            if len(friends)>10:
-                    break
+            #if len(friends)>10:
+                   # break
+
+# Sets
+# fruits={"apple","orange","banana","coconut"}
+#print(dir(fruits))# tells us all the methods that come with it
+#print(help(fruits))#
+#print(len(fruits))# tells us the length
+#print("pineapple" in fruits) #tells us if pineapple is in fruits
+
+#print(fruits[0])#this doesnt work for a set
+# fruits.add("pineapple")# adds fruit
+# fruits.add("strawberry")
+# fruits.add("kiwi")
+# fruits.remove("apple")#removes fruit
+# fruits.pop()# pops off item at the end
+# fruits.clear()#clears/gets rid of all fruits
+#tuples
+#fruits=("apple","orange","banana","coconut")
+#print(dir(fruits))# tells us all the methods that come with it
+#print(help(fruits))#
+#print(len(fruits))# tells us the length
+#print("pineapple" in fruits) #tells us if pineapple is in fruits
+
+#print(fruits.index("apple"))
+#print(fruits.count("coconut"))
+
+# print(fruits)
+# for fruit in fruits
+# print(fruit)
+
+#dictionaries= a collection of {key:value} pairs
+#              ordered and changable. No dupelicates
+
+# capitals = {"USA":"Washignton D.C",
+#             "India":"New Delhi",
+#             "China":"Beijing",
+#             "Russia":"Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("USA")) #Gets you the value of the USA
+# print(capitals.get("India"))
+# print(capitals.get("Japan"))#puts value as name
+
+#if the key is in the dictionary it shows it as existing
+#if capitals.get("USA")
+#print("that capital exists")
+#else:
+# print("that capital doesnt exist")
+
+#capitals.update({"Germany":"Berlin"})#inserts a new key value pair
+# # capitals.update({"usa":"detroit"}) #changes the value of the key
+# # capitals.pop("china") 
+# # capitals.popitem()
+# # capitals.clear
+
+
+
+# keys=capitals.keys()
+# for key in capitals.keys():
+#     print(key)
+
+#     values=capitals.values()
+#     for value in capitals.values():
+#         print(value)
+
+# items=capitals.items()
+# for key, value in capitals.items():
+#     print(f"{key}:{value}")
+
+
+
+
+
